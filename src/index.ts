3d6c37802345db0269978b2d76f28e0e9c5cb68b
nina eberle <eberle@punkt.de>
2020-04-26 12:34:50 +0200

    code comments

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,17 @@ const item = document.getElementsByTagName("li");
 const allow = document.querySelector(".button1");
 const deny = document.querySelector(".button2");
 
+
+// TODO LIST
+// input length of the To Do List
 function inputLength(){
 	return input!.value.length;
 } 
-
 function listLength(){
 	return item.length;
 }
 
+// By the function createListElement, new elemts will be createt in the List
 function createListElement() {
 	const li = document.createElement("li"); // creates an element "li"
 	li.appendChild(document.createTextNode(input!.value)); //makes text from input field the li text
@@ -42,9 +45,10 @@ function createListElement() {
 	function deleteListItem(){
 		li.classList.add("delete")
     }
- 
+}
 
-
+// Items will appear after click
+// to add an item to list, something has to be written in the input field
 function addListAfterClick(){
 	if (inputLength() > 0) { //makes sure that an empty input field doesn't create a li
 		createListElement();
@@ -58,9 +62,10 @@ function addListAfterKeypress(event:any) {
 	} 
 }
 
-
+// functions to add an item to the list
+// after click on enter button list appears and list items are added to list
 enterButton!.addEventListener("click",addListAfterClick);
-
+// after clicking the enter key list appears or items are added to list
 input!.addEventListener("keypress", addListAfterKeypress);
 
 
@@ -75,7 +80,7 @@ input!.addEventListener("keypress", addListAfterKeypress);
 
 
 
-
+// POPUP
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -92,14 +97,14 @@ btn!.onclick = function() {
 
 
 // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
+// span.onclick = function(event:any) {
 //   modal!.style.display = "none";
 // }
 
-// When the user clicks anywhere outside of the modal, close it
+
+// When the user clicks anywhere outside of the modal, it closes
 window.onclick = function(event:any) {
   if (event.target == modal) {
     modal!.style.display = "none";
   }
-}
 }
